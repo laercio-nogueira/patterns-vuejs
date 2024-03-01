3101a28c0d3b3a137be520cf3785b3f677206a1f
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-01 18:35:55 -0300

    chore: test view in return homepage

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import HomePage from '#components/pages/HomePage.vue';
 // eslint-disable-next-line vue/require-name-property
 const vm = new Vue({ // eslint-disable-line no-new
   render: (h) => h(HomePage),
-}).$mount('#main');
+}).$mount('body');
