7387cdd39e7c67a347aa5c4a99bd13c3f883d433
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 16:47:49 -0300

    style: include font-awasome

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import './main/stylesheet/font-awesome.css'
 import HomePage from '#components/pages/HomePage.vue';
 import manageRouters from '#/router/manageRouters';
 
