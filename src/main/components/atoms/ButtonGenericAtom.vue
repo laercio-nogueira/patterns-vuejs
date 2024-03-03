d0f9a56bde5a7815b79f71490544f82ea5657423
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 02:27:20 -0300

    feat: create component Button

---
+++ src/main/components/atoms/ButtonGenericAtom.vue
@@ -0,0 +1,43 @@
+<template>
+  <button 
+    type="submit" 
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonGenericAtom',
+  props: {
+    buttonText: String,
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+button {
+  background-color: $background-primary;
+  color: $color-primary;
+  border: 0;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  // font-size: 1.2rem;
+  // background-color: #055b9b;
+  // color: #fff;
+  // border-radius: 6px;
+  // border: 0px;
+}
+</style>
