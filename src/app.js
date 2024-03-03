50b041e7ee2ceae3ed0cd82aa6810949b652dc15
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 20:14:08 -0300

    fix: fix order import css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import App from '#components/App.vue';
-import manageRouters from '#/router/manageRouters';
-
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import './main/stylesheet/font-awesome.css'
+import App from '#components/App.vue';
+import manageRouters from '#/router/manageRouters';
 
 Vue.use(VueRouter);
 const router = new VueRouter(manageRouters);
