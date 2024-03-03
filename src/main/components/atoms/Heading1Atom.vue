3fad6273937aedba50512080ccff9d205b2a72bc
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 18:28:49 -0300

    feat: create file Heading1Atom

---
+++ src/main/components/atoms/Heading1Atom.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="heading1">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Heading1Atom',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.heading1 {
+  font-size: 28px;
+  font-weight: bold;
+  color: #555;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  line-height: 20px;
+  font-family: $font-default;
+}
+</style>
