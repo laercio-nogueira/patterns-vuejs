000720c87d37d232c23fe5394151fcb6d41a74b1
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 19:12:16 -0300

    feat: create typography heading5

---
+++ src/main/components/atoms/Heading5Atom.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="heading5">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Heading5Atom',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.heading5 {
+  font-size: 16px;
+  color: #07448f;
+  margin: 2px 0px;
+  padding: 0;
+  line-height: 20px;
+  font-family: $font-default;
+}
+</style>
