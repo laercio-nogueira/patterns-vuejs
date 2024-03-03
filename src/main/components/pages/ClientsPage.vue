b5c0ae27218e783269e1c1527dfb3363499ccf44
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 17:52:28 -0300

    feat: create ClientPage

--- src/main/components/pages/ClientsPage.vue
+++ src/main/components/pages/ClientsPage.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    ClientsPage
+    <h1> Clientes </h1>
+    <InputGenericAtom
+      textLabel="Nome"
+      type="text" 
+      name="nome" 
+      placeholder="Preencha o nome" 
+      required
+      value=""
+      @input="(value) => console.log(value)"
+    />
+    <br/>
+    <CheckBoxGenericAtom
+      textLabel="sim"
+      value="sim"
+      @change="(value) => console.log(value)"
+      :checked="false"
+    />
+
+    <CheckBoxGenericAtom
+      textLabel="nao"
+      value="nao"
+      @change="(value) => console.log(value)"
+      checked
+    />
+    <br/>
+    <ButtonGenericAtom
+      buttonText="Enviar"
+      @click="() => alert('Enviado!')"
+    />
+
+    
   </div>
 </template>
 
 <script>
+import ButtonGenericAtom from '../atoms/ButtonGenericAtom'
+import InputGenericAtom from '../atoms/InputGenericAtom'
+import CheckBoxGenericAtom  from '../atoms/CheckBoxGenericAtom'
+
 export default {
-  name: 'ProductsPage',
+  name: 'ClientsPage',
+  components: {
+    ButtonGenericAtom,
+    InputGenericAtom,
+    CheckBoxGenericAtom
+  }
 }
 </script>
