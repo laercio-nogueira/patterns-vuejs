ba54f260f0be26c320036154ba337ac4b4e67b15
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 18:23:32 -0300

    fix: rename file homePage to app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import App from '#components/App.vue';
+import manageRouters from '#/router/manageRouters';
+
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import './main/stylesheet/font-awesome.css'
-import HomePage from '#components/pages/HomePage.vue';
-import manageRouters from '#/router/manageRouters';
 
 Vue.use(VueRouter);
 const router = new VueRouter(manageRouters);
 
 const vm = new Vue({
-  render: (h) => h(HomePage),
+  render: (h) => h(App),
   router,
 }).$mount('body');
---
+++ src/main/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <HeaderGenericAtom logo="Sistema XYZ"/>
+    <MenuSidebar />
+
+    <div class="page-content">
+      <div class="container mt-3">
+        <div class="row">
+          <div class="col-12">
+            <router-view/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderGenericAtom from '#components/atoms/HeaderGenericAtom'
+import MenuSidebar from '#components/molecules/MenuSidebar'
+
+export default {
+  name: 'HomePage',
+  components: {
+    HeaderGenericAtom,
+    MenuSidebar
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.page-content {
+  display: flex;
+  float: right;
+  width: calc(100% - 235px);
+}
+</style>
