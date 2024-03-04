2de49ff2b14bfc9889dfefca972c5caa34afddef
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-03 22:32:50 -0300

    feat: create component List

---
+++ src/main/components/atoms/ListDataAtom.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                  <th v-for="(legend, index) in legends" :key="index">{{legend}}</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in items" :key="index">
+                <td v-for="(legend, index) in legends" :key="index">{{ item[legend] }}</td>
+              </tr>
+            </tbody>
+        </table>
+    </div>    
+</template>
+
+<script>
+export default {
+  props: {
+    legends: Array,
+    items: Array
+  },
+};
+</script>
