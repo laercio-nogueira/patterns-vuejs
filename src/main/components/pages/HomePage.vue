6c73d85fa4560d8c8f23578e94d92aece8a99acb
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-04 01:24:31 -0300

    feat: show clients list

--- src/main/components/pages/HomePage.vue
+++ src/main/components/pages/HomePage.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="card">
-    <Heading1Atom text="Home page" />
+    <Heading1Atom text="Principal" class="mb-4"/>
+
+    <ListDataAtom 
+      :legends="['Name', 'Documento', 'Telefone', 'Email', 'Ativo']"
+      :items="getClients"
+    />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Heading1Atom from '#components/atoms/Heading1Atom'
+import ListDataAtom from '../atoms/ListDataAtom'
 
 export default {
   name: 'HomePage',
   components: {
-    Heading1Atom
+    Heading1Atom,
+    ListDataAtom
+  },
+  computed: {
+    ...mapGetters('clients', ['getClients'])
   }
 }
 </script>
