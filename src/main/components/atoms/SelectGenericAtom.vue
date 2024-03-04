584c182efb61c87be2a56a69fd42954edb1f7ff1
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-04 14:20:48 -0300

    feat: create component legend

---
+++ src/main/components/atoms/SelectGenericAtom.vue
@@ -0,0 +1,23 @@
+<template>
+  <select class="form-select" @input="$emit('input', $event.target.value)">
+    <option selected disabled>Selecione</option>
+    <option 
+      v-for="(item, index) in items" 
+      :key="index"
+      :value="item.id"
+      >
+        {{ item[field] }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'SelectGenericAtom',
+  props: {
+    items: Array,
+    selected: String,
+    field: String,
+  }
+}
+</script>
