567b499f80a035261093c52ddabbfa1f7a408207
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-04 14:22:11 -0300

    feat: create link products by client

--- src/main/store/modules/clients.store.js
+++ src/main/store/modules/clients.store.js
@@ -2,8 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 const state = {
   clients: [
-    { id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3ddddf', Name: 'John Doe', Documento: '123.456.789-01', Telefone: '(12) 934567-8901', Email: 'john.doea@teste.br', Ativo: true },
-    { id: '111d6bcd-bbfd-4b2d-9b5d-998dfbzd4bed', Name: 'Mary Jane', Documento: '987.654.321-00', Telefone: '(45) 967843-2111', Email: 'tteste@rots.com', Ativo: true },
+    { id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3ddddf', Name: 'John Doe', Documento: '123.456.789-01', Telefone: '(12) 934567-8901', Email: 'john.doea@teste.br', Ativo: true, products: [] },
+    { id: '111d6bcd-bbfd-4b2d-9b5d-998dfbzd4bed', Name: 'Mary Jane', Documento: '987.654.321-00', Telefone: '(45) 967843-2111', Email: 'tteste@rots.com', Ativo: true, products: [] },
   ]
 }
 const mutations = {
@@ -11,17 +11,27 @@ const mutations = {
     state.clients.push(client)
   },
   removeClient(state, clients) {
-   
+    state.clients = clients
+  },
+  linkProduct(state, { clientId, product }) {
+    const clients = state.clients.map(item => ({
+      ...item,
+      products: item.id === clientId ? [...item.products, product] : item.products
+    }))
     state.clients = clients
   }
 }
 const actions = {
   addClient({ commit }, client) {
-    commit('addClient', { ...client, id: uuidv4() })
+    commit('addClient', { ...client, id: uuidv4(), products: [] })
   },
   removeClient({ state, commit }, id) {
     const newStateClients = state.clients.filter(item => item.id !== id)
     commit('removeClient', newStateClients)
+  },
+  linkProduct({ rootState, commit }, { productId, clientId }) {
+    const product = rootState.products.items.find(item => item.id === productId)
+    commit('linkProduct', { clientId, product })
   }
 }
 const getters = {
