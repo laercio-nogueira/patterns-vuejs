5c10f9ce045f6134a5470e183ff730b4b9b86094
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-04 14:19:10 -0300

    fix: remove store relationships not used

--- src/main/store/index.js
+++ src/main/store/index.js
@@ -1,13 +1,11 @@
 import VuexPersistence from 'vuex-persist'
 import clients from './modules/clients.store';
 import products from './modules/products.store';
-import relationships from './modules/relationships.store';
 
 const store = {
   modules: {
     clients,
     products,
-    relationships
   },
   plugins: [new VuexPersistence().plugin]
 };
