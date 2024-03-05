5a5d8ec2b20a19e6c70e77b703dcf4e967c5e191
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:02:07 -0300

    test: create test ToggleSwitchAtom

--- src/spec/components/atoms/ToggleSwitchAtom.spec.js
+++ src/spec/components/atoms/ToggleSwitchAtom.spec.js
@@ -1,3 +1,14 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ToggleSwitchAtom from '../../../main/components/atoms/ToggleSwitchAtom.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(ToggleSwitchAtom, {
+  localVue,
+})
+
+describe('ToggleSwitchAtom Component', () => {
+  test('When mounted component expect contain class switch and input type checkbox', async () => {
+    expect(wrapper.find('label').classes()).toEqual(["switch"]);
+    expect(wrapper.find('input').attributes('type')).toEqual("checkbox");
+  })
 })
