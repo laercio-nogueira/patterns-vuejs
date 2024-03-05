3848136901f2b46ca6f4fadb1fd4074ee8791d0c
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:07:30 -0300

    test: create test HanderGeneric Atom

--- src/spec/components/atoms/HeaderGenericAtom.spec.js
+++ src/spec/components/atoms/HeaderGenericAtom.spec.js
@@ -1,3 +1,17 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import HeaderGenericAtom from '../../../main/components/atoms/HeaderGenericAtom.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(HeaderGenericAtom, {
+  localVue,
+})
+
+describe('HeaderGenericAtom Component', () => {
+  test('When mounted component, expect Logo to equal "Logo" and contain page-header, navbar and navbar-fixed-top', async () => {
+    await wrapper.setProps({
+      logo: "Logo"
+    })
+    expect(wrapper.classes()).toEqual(["page-header", "navbar", "navbar-fixed-top"]);
+    expect(wrapper.props().logo).toBe("Logo");
+  })
 })
