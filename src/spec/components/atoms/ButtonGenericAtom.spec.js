1733e8fa02f01d1c7af1dd5d24d275576a2ab438
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:06:42 -0300

    test: create ButtonGenericAtom

--- src/spec/components/atoms/ButtonGenericAtom.spec.js
+++ src/spec/components/atoms/ButtonGenericAtom.spec.js
@@ -1,3 +1,18 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ButttonGenericAtom from '../../../main/components/atoms/ButtonGenericAtom.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(ButttonGenericAtom, {
+  localVue,
+})
+
+describe('ButttonGenericAtom Component', () => {
+  test('When mounted component, expect contain Text-Button, class btn and btn-primary and width 100%', async () => {
+    await wrapper.setProps({ 
+      buttonText: 'Text-Button',
+    })
+    expect(wrapper.find('button').text()).toBe('Text-Button');
+    expect(wrapper.find('button').classes()).toEqual(["btn", "btn-primary"]);
+    expect(wrapper.find('button').attributes().style).toEqual("width: 100%;");
+  })
 })
