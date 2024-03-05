579f1ddf8b45034dd188eaa97a5cee3c06eb403f
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:11:18 -0300

    test: create test InputGenericAtom

--- src/spec/components/atoms/InputGenericAtom.spec.js
+++ src/spec/components/atoms/InputGenericAtom.spec.js
@@ -1,3 +1,25 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueTheMask from 'vue-the-mask';
+import InputGenericAtom from '../../../main/components/atoms/InputGenericAtom.vue'
+
+const localVue = createLocalVue();
+localVue.use(VueTheMask);
+const wrapper = shallowMount(InputGenericAtom, {
+  localVue,
+  propsData: {
+    textLabel: "Mock Input Generic",
+    placeholder: "Mock Placeholder",
+    inputName: "GenericInput",
+    type: "text",
+    value: "Mock Value",
+    required: true,
+    mask: "###.###.###-##"
+  }
+})
+
+describe('InputGenericAtom Component', () => {
+  test('When mounted component expect to equal "Mock Input Generic', async () => {
+    expect(wrapper.html())
+      .toEqual("<div class=\"form__input\"><label for=\"GenericInput\">Mock Input Generic</label> <input type=\"text\" name=\"GenericInput\" placeholder=\"Mock Placeholder\" required=\"required\"></div>");
+  })
 })
