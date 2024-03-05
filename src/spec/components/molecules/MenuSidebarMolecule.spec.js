b82cb3bce0e9e3ab31921913f04fe7b0cc0c5c8a
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:03:16 -0300

    test: create test MenuSideBarMolecule

--- src/spec/components/molecules/MenuSidebarMolecule.spec.js
+++ src/spec/components/molecules/MenuSidebarMolecule.spec.js
@@ -1,3 +1,15 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, mount } from '@vue/test-utils'
+import MenuSidebarMolecule from '../../../main/components/molecules/MenuSidebarMolecule.vue'
+import ItemMenuAtom  from '../../../main/components/atoms/ItemMenuAtom.vue'
+
+const localVue = createLocalVue();
+const wrapper = mount(MenuSidebarMolecule, {
+  localVue,
+})
+
+describe('MenuSidebarMolecule Component', () => {
+  test('When mounted component expect compoents to be rendered and contain 3 ItemMenuAtom', async () => {
+    expect(wrapper.findComponent(ItemMenuAtom).exists()).toBeTruthy();
+    expect(wrapper.findAllComponents(ItemMenuAtom)).toHaveLength(3);
+  })
 })
