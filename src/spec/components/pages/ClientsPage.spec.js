a927d3dbc5c1ff1ac744e230e73c6df0bc82f249
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:13:10 -0300

    test: create test ClientsPage

--- src/spec/components/pages/ClientsPage.spec.js
+++ src/spec/components/pages/ClientsPage.spec.js
@@ -1,3 +1,13 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ClientsPage from '../../../main/components/pages/ClientsPage.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(ClientsPage, {
+  localVue
+})
+
+describe("ClientsPage Component Test", () => {
+  test('When mounted component expect to be rendered', () => {
+    expect(wrapper.exists()).toBe(true);
+  })
 })
