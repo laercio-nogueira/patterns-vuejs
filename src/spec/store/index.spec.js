88a495f7a24f3e72dab454e8369c30e5296cbacd
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:08:10 -0300

    test: create test index.store

--- src/spec/store/index.spec.js
+++ src/spec/store/index.spec.js
@@ -1,3 +1,20 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
-})
+import store from '../../main/store';
+
+jest.mock('../../main/store/modules/clients.store.js');
+jest.mock('../../main/store/modules/products.store.js');
+
+describe('Store Test', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Should validate store', () => {
+    expect(store).toEqual({
+      modules: {
+        clients: expect.any(Object),
+        products: expect.any(Object),
+      },
+      plugins: [expect.any(Function)],
+    });
+  });
+});
