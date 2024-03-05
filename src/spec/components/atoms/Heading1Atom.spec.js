0e15390ccdf19d0d4c7a5912811ee815803f996e
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:08:33 -0300

    test: create test Heading1Atom

--- src/spec/components/atoms/Heading1Atom.spec.js
+++ src/spec/components/atoms/Heading1Atom.spec.js
@@ -1,3 +1,16 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Heading1Atom from '../../../main/components/atoms/Heading1Atom.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(Heading1Atom, {
+  localVue,
+})
+
+describe('Heading1Atom Component', () => {
+  test('When mounted component expect to equal "Mock Test Heading 1"', async () => {
+    await wrapper.setProps({
+      text: "Mock Test Heading 1"
+    })
+    expect(wrapper.html()).toEqual("<p class=\"heading1\">Mock Test Heading 1</p>");
+  })
 })
