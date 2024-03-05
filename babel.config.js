4d6c609da58cff3c4d3061e8e010f011218b4e0e
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 01:30:03 -0300

    chore: config libs to support jest

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,16 @@
-module.exports = {}
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env'
+    ],
+  ],
+  comments: false,
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs',
+  ],
+};
+
