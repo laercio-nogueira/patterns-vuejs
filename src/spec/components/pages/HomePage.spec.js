bfaa9b9df3d9b3ba7c215406d744287ff5bfd07a
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:13:26 -0300

    test: create test HomePage

--- src/spec/components/pages/HomePage.spec.js
+++ src/spec/components/pages/HomePage.spec.js
@@ -1,3 +1,47 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import HomePage from '../../../main/components/pages/HomePage.vue'
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    clients: {
+      namespaced: true,
+      state: {
+        clients: [],
+      },
+      getters: {
+        getClients: state => state.clients
+      },
+      actions: {
+        removeClient: jest.fn()
+      }
+    },
+    products: {
+      namespaced: true,
+      state: {
+        items: []
+      },
+      getters: {
+        getProducts: state => state.items
+      },
+      actions: {
+        addProduct: jest.fn()
+      }
+    }
+  }
+})
+
+
+const wrapper = mount(HomePage, {
+  localVue,
+  store
+})
+
+describe("HomePage Component Test", () => {
+  test('When mounted component expect to be rendered', () => {
+    expect(wrapper.exists()).toBe(true);
+  })
 })
