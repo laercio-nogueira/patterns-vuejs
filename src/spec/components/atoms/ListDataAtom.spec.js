6131acc3dfc55a2d44b4adfa41b45510f0794d33
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:14:03 -0300

    test: create ListDataAtom

--- src/spec/components/atoms/ListDataAtom.spec.js
+++ src/spec/components/atoms/ListDataAtom.spec.js
@@ -1,3 +1,23 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ListDataAtom from '../../../main/components/atoms/ListDataAtom.vue'
+
+const localVue = createLocalVue();
+const wrapper = shallowMount(ListDataAtom, {
+  localVue,
+  propsData: {
+    legends: ['mock-legend1', 'mock-legend2'],
+    items: [{
+      'mock-legend1': 'mock-legend1-value',
+      'mock-legend2': 'mock-legend2-value'
+    }],
+    link: true,
+    remove: true
+  }
+})
+
+describe('ListDataAtom Component', () => {
+  test('When mounted component expect contain class "table table-striped table-bordered and totalColSpan = 4"', () => {
+    expect(wrapper.classes()).toEqual(["table", "table-striped", "table-bordered"]);
+    expect(wrapper.vm.totalColSpan).toEqual(4);
+  })
 })
