857076110b1ac7b89780f7fb151e4a56a5984a3b
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 02:12:41 -0300

    test: create test ItemMenuAtom

--- src/spec/components/atoms/ItemMenuAtom.spec.js
+++ src/spec/components/atoms/ItemMenuAtom.spec.js
@@ -1,3 +1,22 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueTheMask from 'vue-the-mask';
+import ItemMenuAtom from '../../../main/components/atoms/ItemMenuAtom.vue'
+
+const localVue = createLocalVue();
+localVue.use(VueTheMask);
+const wrapper = shallowMount(ItemMenuAtom, {
+  localVue,
+  propsData: {
+    textItem: "Mock Text",
+    page: "/mock-page",
+    icon: "success"
+  }
+})
+
+describe('ItemMenuAtom Component', () => {
+  test('When mounted component expect to equal "Mock Text and contain menu-sidebar__nav-item and fa-success', () => {
+    expect(wrapper.classes()).toEqual(["menu-sidebar__nav-item"]);
+    expect(wrapper.find('i').classes()).toEqual(["fa", "fa-success"]);
+    expect(wrapper.find('span').html()).toBe("<span>Mock Text</span>");
+  })
 })
