ec024bc73d9ae49b7cee0f0e18a14608434ebb26
Laercio Nogueira <nogueira.laercio@hotmail.com>
2024-03-05 07:13:51 -0300

    test: create test ProductsPage

--- src/spec/components/pages/ProductsPage.spec.js
+++ src/spec/components/pages/ProductsPage.spec.js
@@ -1,3 +1,48 @@
-test('Example Test', () => {
-  expect(true).toBe(true);
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueTheMask from 'vue-the-mask';
+import ProductsPage from '../../../main/components/pages/ProductsPage.vue'
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.use(VueTheMask);
+
+const store = new Vuex.Store({
+  modules: {
+    clients: {
+      namespaced: true,
+      state: {
+        clients: [],
+      },
+      getters: {
+        getClients: state => state.clients
+      },
+      actions: {
+        removeClient: jest.fn()
+      }
+    },
+    products: {
+      namespaced: true,
+      state: {
+        items: []
+      },
+      getters: {
+        getProducts: state => state.items
+      },
+      actions: {
+        addProduct: jest.fn()
+      }
+    }
+  }
+})
+
+const wrapper = shallowMount(ProductsPage, {
+  localVue,
+  store
+})
+
+describe("ProductsPage Component Test", () => {
+  test('When mounted component expect to be rendered', () => {
+    expect(wrapper.exists()).toBe(true);
+  })
 })
